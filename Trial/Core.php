90b713f22aae0ee26a3bc1f1770e55310d247337
volter9 <volter925@gmail.com>
2015-02-13 18:09:29 -0800

    Added super-duper amazing class called Core which do amazing stuff as version control...

---
+++ Trial/Core.php
@@ -0,0 +1,26 @@
+<?php namespace Trial;
+
+/**
+ * Core class 
+ * containing information about version
+ * 
+ * @package Trial
+ */
+class Core {
+	
+	/**
+	 * @const string String representation of CMS version
+	 */
+	const VERSION = 'v1.0';
+	
+	/**
+	 * @const int Major version
+	 */
+	const VERSION_MAJOR = 1;
+	
+	/**
+	 * @const int Minor version
+	 */
+	const VERSION_MINOR = 0;
+	
+}
