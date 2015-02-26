45e8db0e14bd1923853678ce9b1d7200dcf3f1f4
volter9 <volter925@gmail.com>
2015-02-25 20:35:44 -0800

    Added 'just in case' charset for MySQL

--- App/Views/blocks/head.php
+++ App/Views/blocks/head.php
@@ -1,2 +1,3 @@
+<meta charset="UTF-8"/>
 <title><?php echo $title ?> - <?php echo $this->lang('website') ?></title>
 <link href="<?php echo $this->asset('css/main.css') ?>" rel="stylesheet" type="text/css"/>
--- Trial/DB/Connection.php
+++ Trial/DB/Connection.php
@@ -29,7 +29,8 @@ class Connection {
 		$options = [
 			PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
 			PDO::ATTR_EMULATE_PREPARES => false,
-			PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+			PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+			PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES {$this->charset['charset']}"
 		];
 		
 		$pdo = new PDO(
