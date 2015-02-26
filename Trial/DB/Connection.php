7e72d9199d7cc097abb03b2cf8de517a8d8af4bc
volter9 <volter925@gmail.com>
2015-02-25 20:39:18 -0800

    Made a typo

--- Trial/DB/Connection.php
+++ Trial/DB/Connection.php
@@ -30,7 +30,7 @@ class Connection {
 			PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
 			PDO::ATTR_EMULATE_PREPARES => false,
 			PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-			PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES {$this->charset['charset']}"
+			PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES {$this->config['charset']}"
 		];
 		
 		$pdo = new PDO(
